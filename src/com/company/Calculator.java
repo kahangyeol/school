6231740592999a3c240a7e8f1e394bb671ec58cc
DESKTOP-4AKTODL\user <rkgksruf321@naver.com>
2022-04-02 15:52:17 +0900

    no message

--- src/com/company/Calculator.java
+++ src/com/company/Calculator.java
@@ -24,6 +24,7 @@ public class Calculator {
                 div(num1,num2);
                 break;
         }
+        scanner.close();
     }
 
     public static void init(){
---
+++ src/com/company/Gugudan.java
@@ -0,0 +1,29 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Gugudan {
+    static int gugudan;
+    static int gugudanCount;
+    static Scanner scanner;
+    public static void main(String[] args) {
+        init();
+        result();
+        scanner.close();
+    }
+
+    public static void init(){
+        scanner = new Scanner(System.in);
+        System.out.print("몇단: ");
+        gugudan = scanner.nextInt();
+
+        System.out.print("몇까지: ");
+        gugudanCount = scanner.nextInt();
+    }
+
+    public static void result(){
+        for(int i = 1; i <= gugudanCount; i++){
+            System.out.println(gugudan + "X" + i + "=" + gugudan*i);
+        }
+    }
+}
