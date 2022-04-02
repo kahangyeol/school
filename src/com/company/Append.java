596a699d996f960eef4d56f258548643096defd1
DESKTOP-4AKTODL\user <rkgksruf321@naver.com>
2022-04-02 16:11:44 +0900

    no message

---
+++ src/com/company/Append.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Append {
+    static Scanner scanner;
+    static String first;
+    static String second;
+
+    public static void main(String[] args) {
+        init();
+        append();
+    }
+
+    public static void init() {
+        scanner = new Scanner(System.in);
+        System.out.print("첫번째 문자열: ");
+        first = scanner.nextLine();
+
+        System.out.print("두번째 문자열: ");
+        second = scanner.nextLine();
+    }
+
+    public static void append() {
+        System.out.println("결과" + first + second);
+    }
+}
---
+++ src/com/company/Triple.java
@@ -0,0 +1,41 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Triple {
+    static Scanner scanner;
+    static int num1;
+    static int num2;
+    static int num3;
+    static int sum;
+
+    public static void main(String[] args) {
+        init();
+        sum = fSum();
+        avg(sum);
+        scanner.close();
+    }
+
+    public static void init(){
+        scanner = new Scanner(System.in);
+        System.out.print("첫번째: ");
+        num1 = scanner.nextInt();
+
+        System.out.print("두번째: ");
+        num2 = scanner.nextInt();
+
+        System.out.print("세번째: ");
+        num3 = scanner.nextInt();
+    }
+
+    public static int fSum(){
+        int sum = num1+num2+num3;
+        System.out.println("합계: " + sum);
+        return sum;
+    }
+
+    public static void avg(int sum){
+        System.out.println("평균: " + sum / 3);
+    }
+
+}
