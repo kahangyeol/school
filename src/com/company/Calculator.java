d1b01fb5912aa0266673feaad6dfb8c66c0d9920
DESKTOP-4AKTODL\user <rkgksruf321@naver.com>
2022-04-02 15:45:21 +0900

    no message

---
+++ src/com/company/Calculator.java
@@ -0,0 +1,54 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Calculator {
+    static Scanner scanner;
+    static int num1;
+    static int num2;
+    static String operator;
+
+    public static void main(String[] args) {
+        init();
+        switch (operator){
+            case "+":
+                sum(num1,num2);
+                break;
+            case "-":
+                min(num1,num2);
+                break;
+            case "*":
+                mok(num1,num2);
+                break;
+            case "/":
+                div(num1,num2);
+                break;
+        }
+    }
+
+    public static void init(){
+        scanner = new Scanner(System.in);
+
+        System.out.print("첫번째 숫자: ");
+        num1 = scanner.nextInt();
+
+        System.out.print("(+,-,/.*) 연산자: ");
+        operator = scanner.next();
+
+        System.out.print("두번째 숫자: ");
+        num2 = scanner.nextInt();
+    }
+
+    public static void sum(int num1, int num2){
+        System.out.println("결과: " + (num1 + num2));
+    }
+    public static void mok(int num1, int num2){
+        System.out.println("결과: " + (num1 * num2));
+    }
+    public static void min(int num1, int num2){
+        System.out.println("결과: " + (num1 - num2));
+    }
+    public static void div(int num1, int num2){
+        System.out.println("결과: " + (num1 / num2));
+    }
+}
