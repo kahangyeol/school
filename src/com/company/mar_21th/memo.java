5f3e2583ecdbecf675a3327ebf9b2b938f9dae86
DESKTOP-4AKTODL\user <rkgksruf321@naver.com>
2022-05-28 16:12:43 +0900

    20220528

--- src/com/company/mar_21th/memo.java
+++ src/com/company/mar_21th/memo.java
@@ -1,12 +1,14 @@
 package com.company.mar_21th;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class memo {
     static Scanner scanner;
     static String name;
     static int rank;
+    static ArrayList<String> arrayMemo;
     static String content;
     static String path = "d:\\hangyeol\\text.txt";
     static File file = new File(path);
@@ -38,6 +40,7 @@ public class memo {
                 memoWrite();
                 break;
             case 2:
+                memoRead();
                 break;
             case 3:
                 break;
@@ -60,16 +63,16 @@ public class memo {
         System.out.print("중요도(1,2,3,...)");
         rank = scanner.nextInt();
 
-        int count  = memoCount();
-        if(count < 10){
-            sCount = "0"+count;
-        }else {
+        int count = memoCount();
+        if (count < 10) {
+            sCount = "0" + count;
+        } else {
             sCount += count;
         }
         try {
             fileWriter = new FileWriter(file, true);
             bufferedWriter = new BufferedWriter(fileWriter);
-            bufferedWriter.write("카운트 " + sCount + ". 이름: " + name + " 중요도: " + rank + ".");
+            bufferedWriter.write("카운트 " + sCount + " 중요도: " + rank + "." + " 이름: " + name);
             bufferedWriter.newLine();
 
 
@@ -104,17 +107,81 @@ public class memo {
         }
     }
 
-    private static void memoRead() {
+    private static void memoRead() {    // File dir() = 파일 리스트
         FileReader fileReader = null;
         BufferedReader bufferedReader = null;
-
+        arrayMemo = new ArrayList<>();
         try {
             fileReader = new FileReader(path);
             bufferedReader = new BufferedReader(fileReader);
             String data;
-            while((data = bufferedReader.readLine()) != null){
-
+            int count = memoCount();
+            int index = 0;
+            while ((data = bufferedReader.readLine()) != null || count > index) {
+                if (data.equals("=======================================")) {
+                    data = bufferedReader.readLine();
+                    if (data.equals("")) {
+                        data = bufferedReader.readLine();
+                    }
+                    if (data == null) {
+                        break;
+                    }
+                    String debug = index + ". " + data.substring(15);
+                    arrayMemo.add(debug);
+                    System.out.println(arrayMemo.get(index));
+                    index++;
+                }
             }
+            System.out.println("골라");
+            int select = scanner.nextInt();
+
+            bufferedReader.close();
+            fileReader.close();
+            fileReader = new FileReader(path);
+            bufferedReader = new BufferedReader(fileReader);
+            while ((data = bufferedReader.readLine()) != null || count > index) {
+                data = bufferedReader.readLine();
+                if (data.equals("")) {
+                    data = bufferedReader.readLine();
+                }
+                if (data == null) {
+                    break;
+                } if (data.equals("=======================================")) {
+                    data = bufferedReader.readLine();
+                    if (data.equals("")) {
+                        data = bufferedReader.readLine();
+                    }
+                    if (data == null) {
+                        break;
+                    }
+                    String debug = data.substring(4,6);
+                    int selectTemp = Integer.parseInt(debug);
+
+                    if(select == selectTemp){
+                        while((data = bufferedReader.readLine()) != null){
+                            System.out.println(data);
+                            if(data.equals("=======================================")){
+                                break;
+                            }
+                        }
+                    }
+
+
+
+                    /*data = bufferedReader.readLine();
+                    if (data.equals("")) {
+                        data = bufferedReader.readLine();
+                    }
+                    if (data == null) {
+                        break;
+                    }
+                    String a = index + ". " + data.substring(15);
+                    arrayMemo.add(a);
+                    System.out.println(arrayMemo.get(index));
+                    index++;*/
+                }
+            }
+
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
